ea3bfc10e4b13dc296c43a74ebdf4ebd9c695aef
Miron Bury <miron.bury@gmail.com>
2022-10-03 17:29:00 +0100

    Initial commit

---
+++ Assets/Scripts/Gun.cs
@@ -0,0 +1,87 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gun : MonoBehaviour
+{
+    [SerializeField]
+    float m_clipSize = 0;
+    [SerializeField]
+    float m_recoil = 0;
+    [SerializeField]
+    float m_damage = 0;
+    [SerializeField]
+    float m_bulletLifeTime = 0;
+
+    [SerializeField]
+    Rigidbody2D m_rb;
+
+    int gunType = 0;
+    void GunType()
+    {
+        switch (gunType)
+        {
+            case 1: // pistol
+                break;
+            case 2: //shotgun
+                break;
+            case 3: //rocket luncher
+                break;
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.touchCount > 0)
+        {
+            Touch touch = Input.GetTouch(0);
+            if (touch.phase == TouchPhase.Began)
+            {
+                Vector3 position = Camera.main.ScreenToWorldPoint(touch.position);
+                Fire(position);
+            }
+        }    
+    }
+
+    private void Fire(Vector3 target)
+    {
+        Debug.DrawLine(transform.position, target, Color.white, 1.0f);
+
+        Vector3 recoil = target.normalized * -1.0f * m_recoil;
+        m_rb.AddForce(recoil);
+    }
+
+    protected void OnGUI()
+    {
+        GUI.skin.label.fontSize = Screen.width / 40;
+
+        if (Input.touchCount > 0)
+        {
+            Touch touch = Input.GetTouch(0);
+            GUILayout.Label("Touch phase: " + touch.phase);
+            GUILayout.Label("Touch screen position: " + touch.position);
+            GUILayout.Label("Touch world position: " + Camera.main.ScreenToWorldPoint(touch.position));
+        }
+
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = new Color(1, 0, 0, 0.5f);
+        Gizmos.DrawCube(transform.position, transform.lossyScale);
+        if (Input.touchCount > 0)
+        {
+            Touch touch = Input.GetTouch(0);
+            Gizmos.color = new Color(0, 1, 0, 0.5f);
+            Gizmos.DrawCube(Camera.main.ScreenToWorldPoint(touch.position), transform.lossyScale);
+        }
+    }
+
+}
---
+++ Assets/Scripts/Slide.cs
@@ -0,0 +1,95 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Slide : MonoBehaviour
+{
+    [SerializeField]
+    float maxVelocity = 0f;
+
+    [SerializeField]
+    private GameObject gun = null;
+
+    [SerializeField]
+    Rigidbody2D rb;
+
+    [SerializeField]
+    float m_maxAngle = 45f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //turn on gyro
+        Input.gyro.enabled = true;
+        //stop screen rotating
+        Screen.orientation = ScreenOrientation.LandscapeLeft;
+        //stop screen sleeping
+        Screen.sleepTimeout = SleepTimeout.NeverSleep;
+
+        if (maxVelocity <= 0)
+        {
+            Debug.LogError("need MaxSpeed <= 0");
+        }
+        if (gun = null)
+        {
+            Debug.LogWarning("Remember to add gun");
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Physics2D.gravity = Down() * 9.81f;
+
+        /*
+        cap speed
+        */
+    }
+
+    protected void OnGUI()
+    {
+        //GUI.skin.label.fontSize = Screen.width / 40;
+
+        //GUILayout.Label("Orientation: " + Screen.orientation);
+        //GUILayout.Label("input.gyro.attitude: " + Input.gyro.attitude);
+        //GUILayout.Label("input.acceleration: " + Input.acceleration);
+        //GUILayout.Label("iphone width/font: " + Screen.width + " : " + GUI.skin.label.fontSize);
+
+        //GUILayout.Label("Input.gyro.attitude.eulerAngles: " + Input.gyro.attitude.eulerAngles);
+        //GUILayout.Label("Down: " + Down());
+        //GUILayout.Label("Physics.gravity: " + Physics.gravity);
+    }
+
+    Vector3 Down()
+    {
+        // Phone down vector (ignoring z copordinate)
+        Vector3 down = Input.acceleration;
+        down.z = 0f;
+
+        // Rotation from unity down to my down
+        Quaternion rotation = Quaternion.FromToRotation(Vector3.down, down);
+
+        //clamp
+        float angle = rotation.eulerAngles.z;
+        if (angle >= 180)
+        {
+            angle -= 360;
+        }
+        angle = Mathf.Clamp(angle, -m_maxAngle, m_maxAngle);
+        
+        // Clamped rotation
+        down = Quaternion.Euler(0, 0, angle) * Vector3.down;
+
+        //debug draw
+        Debug.DrawLine(transform.position, transform.position+down, Color.red);
+        return down;
+    }
+
+
+    /*
+    private static Quaternion GyroToUnity(Quaternion q)
+    {
+        return new Quaternion(q.x, q.y, -q.z, -q.w);
+    }
+    */
+}
