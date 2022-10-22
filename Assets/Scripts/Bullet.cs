a571fda5596e88eb555e79e39263dbc5d1adad93
Miron Bury <miron.bury@gmail.com>
2022-10-22 20:34:14 +0100

    create bullet

---
+++ Assets/Scripts/Bullet.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    [SerializeField]
+    float m_speed = 20f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position += transform.right * m_speed *Time.deltaTime;
+        Debug.DrawRay(transform.position, transform.right);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.tag != "Player")
+        {
+            GameObject.Destroy(gameObject);
+        }
+    }
+}
