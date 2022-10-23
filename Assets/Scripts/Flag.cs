5b73b10f0b52f77464404a3e8295fae9ab1cae2e
Miron Bury <miron.bury@gmail.com>
2022-10-24 00:57:57 +0100

    flag
    used to switch level

---
+++ Assets/Scripts/Flag.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Flag : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Player")
+        {
+            int nextSceneIndex = SceneManager.GetActiveScene().buildIndex + 1;
+            SceneManager.LoadScene(nextSceneIndex);
+        }
+    }
+}
