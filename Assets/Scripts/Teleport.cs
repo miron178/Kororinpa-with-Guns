eb2839af3dcf78e75794670f68d0c0290de2a9e2
Miron Bury <miron.bury@gmail.com>
2022-11-15 00:13:19 +0000

    lv 3

---
+++ Assets/Scripts/Teleport.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Teleport : MonoBehaviour
+{
+    [SerializeField]
+    private Transform m_destination;
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        other.gameObject.transform.position = m_destination.position;
+    }
+}
